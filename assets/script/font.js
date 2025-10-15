ddf8ed74ef85d551ca73b6669e8b9fd27a72718d
sanvi <amdsanve@gmail.com>
2025-10-15 19:52:09 +0600

    Add files via upload

---
+++ assets/script/font.js
@@ -0,0 +1,1 @@
+WebFont.load({google:{families:["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Space Mono:regular,italic,700,700italic","Karla:regular,italic,700,700italic","Frank Ruhl Libre:300,regular,500,700,900","Playfair Display:regular,italic,700,700italic,900,900italic","Lora:regular,italic,700,700italic"]}})
